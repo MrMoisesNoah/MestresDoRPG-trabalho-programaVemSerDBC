668140f7cba7cae4d7b23a529f56d67f34294192
Moises.Noah <moises.noah@dbccompany.com.br>
2022-10-06 20:37:25 -0300

    UPDADE-BRANCH MOISES

--- trabalho-finall/src/Jogador/Jogador.java
+++ trabalho-finall/src/Jogador/Jogador.java
@@ -1,8 +1,44 @@
 package Jogador;
 
+import java.util.List;
+
 public class Jogador {
 
     private String nomeJogador;
     private String idade;
+    private List<Personagem> personagemJogador;
+
+
+
+    Jogador(String nomeJogador, String idade, Personagem personagemJogador){
+        this.nomeJogador = nomeJogador;
+        this.idade = idade;
+        this.personagemJogador.add(personagemJogador);
+
+    }
+    public void imprimirJogador(){
+        System.out.println("NOME DO JOGADOR: " + nomeJogador);
+        System.out.println("IDADE DO JOGADOR: " + idade);
+        System.out.println("----PERSONAGEM-----");
+        System.out.println(personagemJogador.get(i).imprimirPersonagem);
+
+    }
+
+    public String getNomeJogador() {
+        return nomeJogador;
+    }
+
+    public void setNomeJogador(String nomeJogador) {
+        this.nomeJogador = nomeJogador;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
 
 }
--- trabalho-finall/src/Jogador/Mestre.java
+++ trabalho-finall/src/Jogador/Mestre.java
@@ -1,4 +1,39 @@
 package Jogador;
 
+import java.util.List;
+
 public class Mestre {
+    private String nomeMestre;
+    private List<Personagem> npc;
+    private List<Personagem> monstros;
+
+
+    public void imprimirMestre(String nomeMestre){
+        System.out.println("NOME DO MESTRE: " + nomeMestre);
+
+    }
+
+    public void imprimirMonstro(){
+
+    }
+    public void imprimirNPC(){
+
+    }
+
+    public Mestre(){
+
+    }
+    public Mestre(String nomeMestre, Personagem npc, Personagem monstros) {
+        this.nomeMestre = nomeMestre;
+        this.npc.add(npc);
+        this.monstros.add(monstros);
+    }
+
+    public String getNomeMestre() {
+        return nomeMestre;
+    }
+
+    public void setNomeMestre(String nomeMestre) {
+        this.nomeMestre = nomeMestre;
+    }
 }
